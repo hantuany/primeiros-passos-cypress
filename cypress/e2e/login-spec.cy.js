0089041d4a0e08a3244fda8c5d82a7a8e1ab129e
hantuany <hantuanyjose1@hotmail.com>
2024-10-15 19:08:02 -0300

    Change selector to dashboard

--- cypress/e2e/login-spec.cy.js
+++ cypress/e2e/login-spec.cy.js
@@ -5,7 +5,8 @@ const selectorsList = {
   passwordField: "[name='password']",
   loginButton: "[type='submit']",
   sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
-  wrongCredentialAlert: "[role='alert']"
+  wrongCredentialAlert: "[role='alert']",
+  dashboardGrid: ".orangehrm-dashboard-grid"
 }
 
   it('Login - Sucess', () => {
@@ -14,7 +15,7 @@ const selectorsList = {
     cy.get(selectorsList.passwordField).type('admin123')
     cy.get(selectorsList.loginButton).click()
     cy.location ('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
+    cy.get(selectorsList.dashboardGrid)
   })
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
